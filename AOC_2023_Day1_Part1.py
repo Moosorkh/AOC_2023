1cb14638a1a3bf8266ef5ff97f345f5602bccd8e
Fortis Irdmousa <moosorkh@gmail.com>
2023-12-04 18:04:10 -0800

    FIrst commit

---
+++ AOC_2023_Day1_Part1.py
@@ -0,0 +1,20 @@
+
+
+input = ".\input.txt"
+
+lines = []
+with open(input, 'r') as file:
+   for line in file:
+      lines.append(line.strip())
+      
+list = []
+for line in lines:
+  list2 = [char for char in line if char.isdigit()]
+  list.append(list2)
+
+sum = 0
+for i in list:
+   concat_num = int(i[0] + i[-1])
+   sum += concat_num
+
+print(sum) 
